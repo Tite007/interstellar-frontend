5ae89ea67506d38642febdee5354084bf95681d8
Tite007 <113141439+Tite007@users.noreply.github.com>
2024-08-13 11:55:34 -0700

    add edge on the jwt token

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-import { getToken } from 'next-auth/jwt'
+import { getToken } from 'next-auth/jwt/edge'
 import { NextResponse, NextRequest } from 'next/server'
 
 export async function middleware(req: NextRequest) {
