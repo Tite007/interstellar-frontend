c98bb54fa157dcdc62c78dd23a290cef1b4a2221
Tite Sanchez <titesanchez0@gmail.com>
2024-10-05 14:23:35 -0700

    fixed middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,13 +1,14 @@
 import { NextResponse } from 'next/server'
 import { auth } from './app/api/auth/[...nextauth]/route'
 
-export default auth((req) =>{
-const { pathname } = req.nextUrl
-const authentication = req.auth.user?.role;
+export default auth((req) => {
+  const { pathname } = req.nextUrl
+  const user = req.auth?.user // Safely check if auth and user exist
+  const authentication = user?.role
 
   // Protect the admin routes
   if (pathname.startsWith('/admin')) {
-    if (!req.auth || authentication !== 'admin') {
+    if (!user || authentication !== 'admin') {
       const notAuthorizedUrl = new URL('/not-authorized', req.nextUrl.origin)
       return NextResponse.redirect(notAuthorizedUrl)
     }
@@ -15,15 +16,14 @@ const authentication = req.auth.user?.role;
 
   // Protect the customer routes if needed
   if (pathname.startsWith('/customer-profile')) {
-    if (!req.auth || authentication !== 'user') {
+    if (!user || authentication !== 'user') {
       const loginUrl = new URL('/customer/login', req.nextUrl.origin) // Customer login page
       return NextResponse.redirect(loginUrl)
     }
   }
 
   return NextResponse.next()
-
-})  
+})
 
 export const config = {
   matcher: ['/admin/:path*', '/customer-profile'],
